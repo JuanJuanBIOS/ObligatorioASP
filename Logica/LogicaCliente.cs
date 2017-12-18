bee43e3717de7bf1c42c4097656d3e93fffaf094
juapem <juapem@gmail.com>
2017-12-18 15:43:04 +0000

    Ahora si va la logica

---
+++ Logica/LogicaCliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Persistencia;
+using EntidadesCompartidas;
+
+namespace Logica
+{
+    public class LogicaCliente
+    {
+        public static Cliente Buscar(int cedula)
+        {
+            return PersistenciaCliente.Buscar(cedula);
+        }
+
+    }
+}
